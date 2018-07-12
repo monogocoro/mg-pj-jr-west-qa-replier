63c054d563ac1bbd4a989b11736cbea0026dd200
ry-tanaka <tanaka@monogocoro.co.jp>
2018-07-12 14:23:15 +0900

    launchable_server

--- server/server.js
+++ server/server.js
@@ -1,29 +1,35 @@
-'use strict';
+// 'use strict';
+//
+// var loopback = require('loopback');
+// var boot = require('loopback-boot');
+//
+// var app = module.exports = loopback();
+//
+// app.start = function() {
+//   // start the web server
+//   return app.listen(function() {
+//     app.emit('started');
+//     var baseUrl = app.get('url').replace(/\/$/, '');
+//     console.log('Web server listening at: %s', baseUrl);
+//     if (app.get('loopback-component-explorer')) {
+//       var explorerPath = app.get('loopback-component-explorer').mountPath;
+//       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+//     }
+//   });
+// };
+//
+// // Bootstrap the application, configure models, datasources and middleware.
+// // Sub-apps like REST API are mounted via boot scripts.
+// boot(app, __dirname, function(err) {
+//   if (err) throw err;
+//
+//   // start the server if `$ node server.js`
+//   if (require.main === module)
+//     app.start();
+// });
+const express = require('express')
+const app = express()
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+app.get('/', (req, res) => res.send('Hello World!'))
 
-var app = module.exports = loopback();
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
