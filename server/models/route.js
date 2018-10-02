db383aeb221caf8e34ed213cb4a78322527d4fef
中島 和希 <nakajima@NT-NAKAJIMA.local>
2018-10-02 13:04:45 +0900

    Fixed for #231

--- server/models/route.js
+++ server/models/route.js
@@ -1,7 +1,8 @@
 'use strict';
 require
 // import {interpreter} from '../jrw.js';
-var interpreter = require('../jrw.js');
+//var interpreter = require('../jrw.js');
+var interpreter = require('../../JRW-NLU/jrw');
 
 module.exports = function(Route) {
   Route.prototype.interpret = function(text) {
